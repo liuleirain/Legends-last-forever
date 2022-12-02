6391d5cc09868e766cee8dc87c122bef87db134d
liuleirain <14699396@qq.com>
2022-12-02 21:34:49 +0800

    Add Some Note

--- Models/Enums/BuffEffectType.cs
+++ Models/Enums/BuffEffectType.cs
@@ -2,17 +2,17 @@
 
 namespace GameServer.Templates
 {
-	[Flags]
-	public enum BuffEffectType
-	{
-		SkillSign = 0,
-		StatusFlag = 1,
-		CausesSomeDamages = 2,
-		StatsIncOrDec = 4,
-		DamageIncOrDec = 8,
-		CreateTrap = 16,
-		LifeRecovery = 32,
-		TemptationBoost = 64,
-		Riding = 128
-	}
+  [Flags]
+  public enum BuffEffectType
+  {
+    SkillSign = 0, //技能标志
+    StatusFlag = 1, //状态标志
+    CausesSomeDamages = 2, //造成伤害
+    StatsIncOrDec = 4, //属性增减
+    DamageIncOrDec = 8, //伤害增减
+    CreateTrap = 16, //创建陷阱
+    LifeRecovery = 32, //生命回复
+    TemptationBoost = 64, //诱惑提升
+    Riding = 128 //坐骑状态
+  }
 }
