31b21fecd18494deede378ffcc4118d4ad4f9bb5
liuleirain <14699396@qq.com>
2022-12-03 22:31:57 +0800

    Add Note

--- Models/Enums/PendingRename55.cs
+++ Models/Enums/PendingRename55.cs
@@ -2,20 +2,20 @@
 
 namespace GameServer.Templates
 {
-	
-	public enum 技能锁定类型
-	{
-		
-		锁定ItSelf,
-		
-		锁定目标,
 
-		锁定ItSelf坐标, //锁定自身坐标
-		
-		锁定目标坐标,
-		
-		锁定锚点坐标,
-		
-		放空锁定ItSelf
-	}
+  public enum 技能锁定类型
+  {
+
+    锁定ItSelf, //锁定自身
+
+    锁定目标,
+
+    锁定ItSelf坐标, //锁定自身坐标
+
+    锁定目标坐标,
+
+    锁定锚点坐标,
+
+    放空锁定ItSelf //放空锁定自身
+  }
 }
